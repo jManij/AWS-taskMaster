dd81b370fd5512f8a0b63abdaa1a1960bb7d92e7
Manish KC <kcmanish89@gmail.com>
2019-09-12 11:46:14 -0700

    Working deployment

--- taskMaster/src/main/java/com/example/taskMaster/Controllers/TaskController.java
+++ taskMaster/src/main/java/com/example/taskMaster/Controllers/TaskController.java
@@ -1,4 +1,81 @@
 package com.example.taskMaster.Controllers;
 
+import com.example.taskMaster.Models.History;
+import com.example.taskMaster.Models.States;
+import com.example.taskMaster.Models.Task;
+import com.example.taskMaster.Repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/taskmaster/v1")
 public class TaskController {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    @GetMapping("/tasks")
+    public List<Task> Tasks() {
+        return (List) taskRepository.findAll();
+    }
+
+
+    @GetMapping("/users/{name}/tasks")
+    public List<Task> getAllTasksForTheUser(@PathVariable String name) {
+        return (List) taskRepository.findByAssignee(name);
+    }
+
+
+    @PostMapping("/tasks")
+    public String addNewtask(@RequestBody Task task) {
+        Task t = new Task();
+        if (task.getAssignee() != null) {
+            t.setAssignee(task.getAssignee());
+            t.setStatus("Assigned");
+        } else {
+            t.setAssignee("");
+            t.setStatus("Available");
+        }
+        t.setTitle(task.getTitle());
+        t.setDescription(task.getDescription());
+        ArrayList<History> historyArrayList = new ArrayList<>();
+        t.setHistoryArrayList(historyArrayList);
+        t.setNewHistory(new History("Task: " + t.getDescription() + " was added"));
+
+        taskRepository.save(t);
+        return "Successfully added a new task!";
+    }
+
+    @PutMapping("/tasks/{id}/state")
+    public String advanceTask(@PathVariable String id) {
+        Task task = taskRepository.findOneById(id);
+        StringBuilder message = new StringBuilder("Task advanced from " + task.getStatus()); // For displaying message
+        task.setStatus(States.changeState(task.getStatus())); //Changes the current status
+        message.append(" to "+ task.getStatus());
+
+        task.getHistoryArrayList().add(new History(message.toString()));
+        taskRepository.save(task);
+        return message.toString();
+
+    }
+
+
+    @PutMapping("/tasks/{id}/assign/{assignee}")
+    public String addUserToTask(@PathVariable String id, @PathVariable String assignee) {
+        Task task = taskRepository.findOneById(id);
+        //Check condition here to see other task status
+
+        task.setAssignee(assignee);
+        task.setStatus("Assigned");
+        task.getHistoryArrayList().add(new History(assignee + " assigned to the task"));
+        taskRepository.save(task);
+
+        return "task assigned to "+ assignee;
+    }
+
+
 }
--- taskMaster/src/main/java/com/example/taskMaster/TaskMasterApplication.java
+++ taskMaster/src/main/java/com/example/taskMaster/TaskMasterApplication.java
@@ -1,13 +1,21 @@
 package com.example.taskMaster;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskMasterApplication {
 
+	@Value("${amazon.aws.accesskey}")
+	private static String amazonAWSAccessKey;
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(TaskMasterApplication.class, args);
+		System.out.println("Task Master is Listening now!!");
 	}
 
 }
+
